8627f1932bd295c8f810d3365500eb0d9815946e
suhha987 <hseo@stanford.edu>
2016-04-04 14:08:11 -0700

    Added files via upload

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-javac -g  -cp external_jars/java-json.jar:external_jars src/Insight.java
-java -cp external_jars/java-json.jar:./src/ Insight
+javac -g  -cp external_jars/java-json.jar:external_jars src/Insight/insight.java
+java -cp external_jars/java-json.jar:./src/ Insight.insight
+
